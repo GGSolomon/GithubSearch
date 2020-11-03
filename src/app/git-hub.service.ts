5abf161fe4d05d68595a94c56af695ae3b5d3c85
GGSolomon <solomonggichuru@gmail.com>
2020-11-03 15:00:11 +0300

    Configure github services

--- src/app/git-hub.service.ts
+++ src/app/git-hub.service.ts
@@ -1,9 +1,59 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError as observableThrowError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+
+// Import interfaces
+import { IUsersList, IGithubUser } from '../interfaces/github-users';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHubService {
+  private username = 'ggsolomon';
+  private client_id = '726b38fda4b044daac69';
+  private client_secret: any = ' 407f535aaa5fae9fc208acb4baf8f25b02864027';
+  private base_url: string = 'https://api.github.com';
+  private api_v3: string = 'application/vnd.github.v3+json';
+  private search_limit:number = 900000; // users search limit
 
-  constructor() { }
+
+  constructor(private http: HttpClient) { }
+
+  
+  // https://api.github.com/search/users?q=tom&per_page=5
+  getUsersList(username: string): Observable<Array<IUsersList>> {
+    return this.http.get<any>(`${this.base_url}/search/users?q=${username}&per_page=${this.search_limit}`, 
+      { headers: { 'Accept': this.api_v3 } }).pipe(
+        map(result => result.items),
+        catchError(this.errorHandler));
+  }
+
+  // https://api.github.com/users/tom
+  getUser(username: string): Observable<IGithubUser> {
+    return this.http.get<IGithubUser>(`${this.base_url}/users/${username}`, 
+      { headers: { 'Accept': this.api_v3 } }).pipe(
+        catchError(this.errorHandler));
+  }
+
+  // https://api.github.com/users/tom/repos
+  getUserRepos(username: string): Observable<any> {
+    return this.http.get<any>(`${this.base_url}/users/${username}/repos`, 
+      { headers: { 'Accept': this.api_v3 } }).pipe(
+        catchError(this.errorHandler));
+  }
+
+  // https://api.github.com/users/tom/followers
+  getUserFollowers(username: string): Observable<any> {
+    return this.http.get<any>(`${this.base_url}/users/${username}/followers`, 
+      { headers: { 'Accept': this.api_v3 } }).pipe(
+        catchError(this.errorHandler));
+  }
+  
+  errorHandler(error: HttpErrorResponse){
+    return observableThrowError(error);
+  } 
+
+
 }
