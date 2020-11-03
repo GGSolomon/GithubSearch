f2591ee60607f943ddbe84676ecda137aaff5b11
GGSolomon <solomonggichuru@gmail.com>
2020-11-03 14:56:37 +0300

    Configure user search list

--- src/app/search-results-item/search-results-item.component.ts
+++ src/app/search-results-item/search-results-item.component.ts
@@ -1,11 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+// Import interfaces
+import { IUserItem } from '../interfaces/github-users';
+
 
 @Component({
   selector: 'app-search-results-item',
   templateUrl: './search-results-item.component.html',
-  styleUrls: ['./search-results-item.component.css']
+  styleUrls: ['./search-results-item.component.scss']
 })
 export class SearchResultsItemComponent implements OnInit {
+  
+  @Input('user_item')  user: any;
 
   constructor() { }
 
--- src/app/search-results-list/search-results-list.component.ts
+++ src/app/search-results-list/search-results-list.component.ts
@@ -1,11 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+// Import interfaces
+import { IUsersList } from '../interfaces/github-users';
 
 @Component({
   selector: 'app-search-results-list',
   templateUrl: './search-results-list.component.html',
-  styleUrls: ['./search-results-list.component.css']
+  styleUrls: ['./search-results-list.component.scss']
 })
 export class SearchResultsListComponent implements OnInit {
+
+  @Input('users') public users: IUsersList;
 
   constructor() { }
 
